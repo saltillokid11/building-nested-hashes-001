02a0ad5c80f7d20f29030b6130672283a19c2943
Tanner Lancaster <tanner@flatironschool.com>
2019-01-24 01:08:16 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,9 +5,11 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        :name => {},
-        :age => {},
-        :attitude => {}
+        {
+          :name => "",
+          :age => "",
+          :attitude => ""
+        }
       ]
    },
    :capulet => {
