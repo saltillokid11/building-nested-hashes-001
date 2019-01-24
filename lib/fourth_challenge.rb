a88565e8c34b831f9e3f5f18c9beeabc1c76485a
Tanner Lancaster <tanner@flatironschool.com>
2019-01-24 01:10:04 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -23,9 +23,14 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         {
-          :name => "",
-          :age => "",
-          :attitude => ""
+          :name => "Steven",
+          :age => "30",
+          :attitude => "confused"
+        },
+        {
+          :name => "Mercutio",
+          :age => "178",
+          :attitude => "hot-headed"
         }
       ]
    }
