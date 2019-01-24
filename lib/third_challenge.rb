4350f63dc2c681852102d84025042c400380a082
Tanner Lancaster <tanner@flatironschool.com>
2019-01-24 01:02:24 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -10,8 +10,8 @@ def third_challenge
         :age = "54"
         },
       :hero => {
-        :name = "Lady Montague",
-        :age = "54",
+        :name = "Romeo",
+        :age = "15",
         :status = "alive"
         },
       :hero_friends => []
