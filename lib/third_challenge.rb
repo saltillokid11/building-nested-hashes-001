a6798e1278f3c602a034e2a25a06b8aec451e5a4
Tanner Lancaster <tanner@flatironschool.com>
2019-01-24 01:02:49 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -18,7 +18,8 @@ def third_challenge
    },
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        :name = "Lord Capulet",
+        :age = "53"
         },
       :matriarch => {
         #your key/value pairs here
